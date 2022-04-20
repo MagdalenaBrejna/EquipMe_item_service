c2126e0f693bf78b23724ee186c82dc0be5f9662
MagdalenaBrejna <magdalena.brejna@wp.pl>
2022-04-20 21:19:28 +0200

    feat: Initial commit

---
+++ src/main/java/mb/equipme_item_service/EquipMeItemServiceApplication.java
@@ -0,0 +1,13 @@
+package mb.equipme_item_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipMeItemServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EquipMeItemServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mb/equipme_item_service/EquipMeItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mb.equipme_item_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipMeItemServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
